89a4441c4dbf21e41328026b0124a31a36a44594
Syahrul Safarila <sysafarila.official@gmail.com>
2024-11-25 20:07:10 +0700

    Revert "Delete donateController.ts"
    This reverts commit c271e5c9dae9365d626ddf4a2419444c963b95f4.

--- src/routes/donations.ts
+++ src/routes/donations.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import DonateController from "../controllers/donateController";
+import DonateController from "../controllers/DonateController";
 import authMiddleware from "../middlewares/authMiddleware";
 
 const router = express.Router();
