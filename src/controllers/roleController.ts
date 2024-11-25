558256651f0c1b8af05e5c0ec716c2496b562ec8
Syahrul Safarila <sysafarila.official@gmail.com>
2024-11-25 19:42:53 +0700

    update role controller

--- src/controllers/roleController.ts
+++ src/controllers/roleController.ts
@@ -14,95 +14,37 @@ import errorHandler from "../utils/errorHandler";
 import HTTPError from "../utils/HTTPError";
 import { validateStore, validateUpdate } from "../validator/validateRole";
 
-export const storeRole = async (req: Request, res: Response) => {
-  const { name, level, permissions } = req.body as RoleCreate;
-  const { role_level_peak } = res.locals as Locals;
-  const prisma = new PrismaClient();
-  try {
-    await validateStore({
-      level: level,
-      name: name,
-      permissions: permissions,
-    });
-
-    if (role_level_peak && role_level_peak >= level) {
-      throw new HTTPError(
-        `Your peak role level is ${role_level_peak}, you cannot create role with level that higher than your level. Note: lower is higher (1 > 2)`,
-        400
-      );
-    }
-
-    const check = await prisma.role.findFirst({
-      where: {
-        name: name,
-      },
-    });
-
-    if (check) {
-      throw new HTTPError("Role already exists", 400);
-    }
-
-    const validPermissions: { name: string }[] = [];
-
-    const checkValidPermissions = await prisma.permission.findMany({
-      where: {
-        name: {
-          in: permissions,
-        },
-      },
-    });
-
-    checkValidPermissions.forEach((permission) => {
-      validPermissions.push({ name: permission.name });
-    });
-
-    const role = await prisma.role.create({
-      data: {
-        id: UUIDV7(),
-        name: name,
+export default class RoleController {
+  static async storeRole(req: Request, res: Response) {
+    const { name, level, permissions } = req.body as RoleCreate;
+    const { role_level_peak } = res.locals as Locals;
+    const prisma = new PrismaClient();
+    try {
+      await validateStore({
         level: level,
-        permissions: {
-          connect: validPermissions,
+        name: name,
+        permissions: permissions,
+      });
+
+      if (role_level_peak && role_level_peak >= level) {
+        throw new HTTPError(
+          `Your peak role level is ${role_level_peak}, you cannot create role with level that higher than your level. Note: lower is higher (1 > 2)`,
+          400
+        );
+      }
+
+      const check = await prisma.role.findFirst({
+        where: {
+          name: name,
         },
-      },
-    });
+      });
 
-    const response: RoleCreateSuccess = {
-      message: "Success",
-      data: {
-        id: role.id,
-        name: role.name,
-        level: role.level,
-        created_at: role.created_at,
-        updated_at: role.updated_at,
-      },
-    };
+      if (check) {
+        throw new HTTPError("Role already exists", 400);
+      }
 
-    res.json(response);
-    return;
-  } catch (error: any) {
-    const handler = errorHandler(error);
+      const validPermissions: { name: string }[] = [];
 
-    res.status(handler.code).json({
-      message: handler.message,
-    } as ErrorResponse);
-  }
-};
-
-export const updateRole = async (req: Request, res: Response) => {
-  const { name, level, permissions } = req.body as RoleUpdate;
-  const { role_level_peak } = res.locals as Locals;
-  const params = req.params as { roleName: string };
-  const prisma = new PrismaClient();
-  try {
-    await validateUpdate({
-      name: name,
-      level: level,
-      permissions: permissions,
-    });
-
-    const validPermissions: { name: string }[] = [];
-    if (permissions) {
       const checkValidPermissions = await prisma.permission.findMany({
         where: {
           name: {
@@ -114,58 +56,213 @@ export const updateRole = async (req: Request, res: Response) => {
       checkValidPermissions.forEach((permission) => {
         validPermissions.push({ name: permission.name });
       });
+
+      const role = await prisma.role.create({
+        data: {
+          id: UUIDV7(),
+          name: name,
+          level: level,
+          permissions: {
+            connect: validPermissions,
+          },
+        },
+      });
+
+      const response: RoleCreateSuccess = {
+        message: "Success",
+        data: {
+          id: role.id,
+          name: role.name,
+          level: role.level,
+          created_at: role.created_at,
+          updated_at: role.updated_at,
+        },
+      };
+
+      res.json(response);
+      return;
+    } catch (error: any) {
+      const handler = errorHandler(error);
+
+      res.status(handler.code).json({
+        message: handler.message,
+      } as ErrorResponse);
     }
+  }
 
-    const check = await prisma.role.findFirst({
-      where: {
-        name: params.roleName,
-      },
-    });
+  static async updateRole(req: Request, res: Response) {
+    const { name, level, permissions } = req.body as RoleUpdate;
+    const { role_level_peak } = res.locals as Locals;
+    const params = req.params as { roleName: string };
+    const prisma = new PrismaClient();
+    try {
+      await validateUpdate({
+        name: name,
+        level: level,
+        permissions: permissions,
+      });
 
-    if (!check) {
-      throw new HTTPError("Role not found", 404);
-    }
+      const validPermissions: { name: string }[] = [];
+      if (permissions) {
+        const checkValidPermissions = await prisma.permission.findMany({
+          where: {
+            name: {
+              in: permissions,
+            },
+          },
+        });
 
-    if (role_level_peak && role_level_peak >= check.level) {
-      throw new HTTPError(
-        `Your peak role level is ${role_level_peak}, you cannot update role with level that higher than your level (${check.level}). Note: lower is higher (1 > 2)`,
-        400
-      );
-    }
+        checkValidPermissions.forEach((permission) => {
+          validPermissions.push({ name: permission.name });
+        });
+      }
 
-    if (role_level_peak && level && role_level_peak >= level) {
-      throw new HTTPError(
-        `Your peak role level is ${role_level_peak}, you cannot update role with level that higher than your level (${check.level}). Note: lower is higher (1 > 2)`,
-        400
-      );
-    }
+      const check = await prisma.role.findFirst({
+        where: {
+          name: params.roleName,
+        },
+      });
 
-    const updateRole = await prisma.role.update({
-      where: {
-        name: params.roleName,
-      },
-      data: {
-        name: name ?? check.name,
-        level: level ?? check.level,
-      },
-    });
+      if (!check) {
+        throw new HTTPError("Role not found", 404);
+      }
 
-    if (permissions) {
-      const updateRolePermissions = await prisma.role.update({
+      if (role_level_peak && role_level_peak >= check.level) {
+        throw new HTTPError(
+          `Your peak role level is ${role_level_peak}, you cannot update role with level that higher than your level (${check.level}). Note: lower is higher (1 > 2)`,
+          400
+        );
+      }
+
+      if (role_level_peak && level && role_level_peak >= level) {
+        throw new HTTPError(
+          `Your peak role level is ${role_level_peak}, you cannot update role with level that higher than your level (${check.level}). Note: lower is higher (1 > 2)`,
+          400
+        );
+      }
+
+      const updateRole = await prisma.role.update({
         where: {
           name: params.roleName,
         },
         data: {
-          permissions: {
-            set: validPermissions,
-          },
+          name: name ?? check.name,
+          level: level ?? check.level,
         },
-        select: {
-          name: true,
-          id: true,
-          level: true,
-          created_at: true,
-          updated_at: true,
+      });
+
+      if (permissions) {
+        const updateRolePermissions = await prisma.role.update({
+          where: {
+            name: params.roleName,
+          },
+          data: {
+            permissions: {
+              set: validPermissions,
+            },
+          },
+          select: {
+            name: true,
+            id: true,
+            level: true,
+            created_at: true,
+            updated_at: true,
+            permissions: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        });
+
+        const response: RoleUpdateSuccess = {
+          message: "Success",
+          data: {
+            id: updateRolePermissions.id,
+            name: updateRolePermissions.name,
+            level: updateRolePermissions.level,
+            created_at: updateRolePermissions.created_at,
+            updated_at: updateRolePermissions.updated_at,
+            permissions: updateRolePermissions.permissions,
+          },
+        };
+
+        res.json(response);
+        return;
+      }
+
+      const response: RoleUpdateSuccess = {
+        message: "Success",
+        data: {
+          id: updateRole.id,
+          name: updateRole.name,
+          level: updateRole.level,
+          created_at: updateRole.created_at,
+          updated_at: updateRole.updated_at,
+        },
+      };
+
+      res.json(response);
+      return;
+    } catch (error: any) {
+      const handler = errorHandler(error);
+
+      res.status(handler.code).json({
+        message: handler.message,
+      } as ErrorResponse);
+    }
+  }
+
+  static async deleteRole(req: Request, res: Response) {
+    const { role_level_peak } = res.locals as Locals;
+    const params = req.params as { roleName: string };
+    const prisma = new PrismaClient();
+
+    try {
+      const check = await prisma.role.findFirst({
+        where: {
+          name: params.roleName,
+        },
+      });
+
+      if (!check) {
+        throw new HTTPError("Role not found", 404);
+      }
+
+      if (role_level_peak && role_level_peak >= check.level) {
+        throw new HTTPError(
+          `Your peak role level is ${role_level_peak}, you cannot delete role with level that higher than your level. Note: lower is higher (1 > 2)`,
+          400
+        );
+      }
+
+      await prisma.role.delete({
+        where: {
+          name: params.roleName,
+        },
+      });
+
+      res.json({
+        message: "Success",
+      } as RoleDeleteSuccess);
+      return;
+    } catch (error: any) {
+      const handler = errorHandler(error);
+
+      res.status(handler.code).json({
+        message: handler.message,
+      } as ErrorResponse);
+    }
+  }
+
+  static async readRole(req: Request, res: Response) {
+    const prisma = new PrismaClient();
+    try {
+      const roles = await prisma.role.findMany({
+        orderBy: {
+          name: "asc",
+        },
+        include: {
           permissions: {
             select: {
               name: true,
@@ -174,112 +271,17 @@ export const updateRole = async (req: Request, res: Response) => {
         },
       });
 
-      const response: RoleUpdateSuccess = {
+      res.json({
         message: "Success",
-        data: {
-          id: updateRolePermissions.id,
-          name: updateRolePermissions.name,
-          level: updateRolePermissions.level,
-          created_at: updateRolePermissions.created_at,
-          updated_at: updateRolePermissions.updated_at,
-          permissions: updateRolePermissions.permissions,
-        },
-      };
-
-      res.json(response);
+        data: roles,
+      } as RoleReadSuccess);
       return;
+    } catch (error: any) {
+      const handler = errorHandler(error);
+
+      res.status(handler.code).json({
+        message: handler.message,
+      } as ErrorResponse);
     }
-
-    const response: RoleUpdateSuccess = {
-      message: "Success",
-      data: {
-        id: updateRole.id,
-        name: updateRole.name,
-        level: updateRole.level,
-        created_at: updateRole.created_at,
-        updated_at: updateRole.updated_at,
-      },
-    };
-
-    res.json(response);
-    return;
-  } catch (error: any) {
-    const handler = errorHandler(error);
-
-    res.status(handler.code).json({
-      message: handler.message,
-    } as ErrorResponse);
   }
-};
-
-export const deleteRole = async (req: Request, res: Response) => {
-  const { role_level_peak } = res.locals as Locals;
-  const params = req.params as { roleName: string };
-  const prisma = new PrismaClient();
-
-  try {
-    const check = await prisma.role.findFirst({
-      where: {
-        name: params.roleName,
-      },
-    });
-
-    if (!check) {
-      throw new HTTPError("Role not found", 404);
-    }
-
-    if (role_level_peak && role_level_peak >= check.level) {
-      throw new HTTPError(
-        `Your peak role level is ${role_level_peak}, you cannot delete role with level that higher than your level. Note: lower is higher (1 > 2)`,
-        400
-      );
-    }
-
-    await prisma.role.delete({
-      where: {
-        name: params.roleName,
-      },
-    });
-
-    res.json({
-      message: "Success",
-    } as RoleDeleteSuccess);
-    return;
-  } catch (error: any) {
-    const handler = errorHandler(error);
-
-    res.status(handler.code).json({
-      message: handler.message,
-    } as ErrorResponse);
-  }
-};
-
-export const readRole = async (req: Request, res: Response) => {
-  const prisma = new PrismaClient();
-  try {
-    const roles = await prisma.role.findMany({
-      orderBy: {
-        name: "asc",
-      },
-      include: {
-        permissions: {
-          select: {
-            name: true,
-          },
-        },
-      },
-    });
-
-    res.json({
-      message: "Success",
-      data: roles,
-    } as RoleReadSuccess);
-    return;
-  } catch (error: any) {
-    const handler = errorHandler(error);
-
-    res.status(handler.code).json({
-      message: handler.message,
-    } as ErrorResponse);
-  }
-};
+}
--- src/routes/roles.ts
+++ src/routes/roles.ts
@@ -1,19 +1,22 @@
 import express from "express";
-import {
-  deleteRole,
-  readRole,
-  storeRole,
-  updateRole,
-} from "../controllers/roleController";
+import RoleController from "../controllers/roleController";
 import authMiddleware from "../middlewares/authMiddleware";
 import can from "../middlewares/permissionMiddleware";
 
 const router = express.Router();
 
 router.use("/roles", authMiddleware);
-router.post("/roles", can("roles-create"), storeRole);
-router.patch("/roles/:roleName", can("roles-update"), updateRole);
-router.delete("/roles/:roleName", can("roles-delete"), deleteRole);
-router.get("/roles", can("roles-read"), readRole);
+router.post("/roles", can("roles-create"), RoleController.storeRole);
+router.patch(
+  "/roles/:roleName",
+  can("roles-update"),
+  RoleController.updateRole
+);
+router.delete(
+  "/roles/:roleName",
+  can("roles-delete"),
+  RoleController.deleteRole
+);
+router.get("/roles", can("roles-read"), RoleController.readRole);
 
 export default router;
