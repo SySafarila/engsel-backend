04d970868192b8c325e159821a13b4052d77332a
Syahrul Safarila <sysafarila.official@gmail.com>
2024-12-30 22:49:06 +0700

    add detail regex

--- src/validator/ValidateAuth.ts
+++ src/validator/ValidateAuth.ts
@@ -19,6 +19,13 @@ export default class ValidateAuth {
 
   static async register(values: Register) {
     const regex = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/; // regex for username, source: https://regexr.com/3cg7r
+    // username harus memenuhi ketentuan berikut
+    // 1. tidak boleh mengandung dua titik berturut-turut (..)
+    // 2. tidak boleh diakhiri dengan titik (.)
+    // 3. harus diawali dengan karakter huruf, angka, atau underscore
+    // 4. hanya boleh mengandung huruf, angka, titik, atau underscore
+    // 5. panjang username maksimal 30 karakter
+
     const schema: Joi.ObjectSchema<Register> = Joi.object({
       email: Joi.string().email().required(),
       password: Joi.string().min(8).required(),
