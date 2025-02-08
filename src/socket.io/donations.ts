887f859cd29e40c834bf1c02820859b981cb8215
Syahrul Safarila <sysafarila.official@gmail.com>
2025-02-08 12:21:49 +0700

    disconnect unknown user_id from web socket

--- src/socket.io/donations.ts
+++ src/socket.io/donations.ts
@@ -1,11 +1,28 @@
+import { PrismaClient } from "@prisma/client";
 import { Socket } from "socket.io";
 
 export const onConnection = (socket: Socket) => {
   console.log(`${socket.id} connected on /donations namespace`);
 
-  socket.on("join", (userId: string) => {
-    socket.join(userId);
-    socket.emit("join", `Joined room:${userId}`);
+  socket.on("join", async (userId: string) => {
+    const prisma = new PrismaClient();
+
+    try {
+      const user = await prisma.user.findFirst({
+        where: {
+          id: userId,
+        },
+      });
+
+      if (user) {
+        socket.join(user.id);
+        socket.emit("join", `Joined room:${userId}`);
+      } else {
+        throw new Error("User not found");
+      }
+    } catch (error) {
+      socket.disconnect(true);
+    }
   });
 
   socket.on("disconnect", (reason) => {
